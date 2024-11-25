c3bfb4440538a6ebf246eb78be4ce40f8a3bd1ad
Pol van Rijn <pol.van-rijn@ae.mpg.de>
2024-11-25 22:10:29 +0100

    Better folder names

--- youtube.py
+++ youtube.py
@@ -25,8 +25,8 @@ def _make_output_dir(folder: str):
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
 
-def download_urls(urls: List, folder: str):
-    output_dir = _make_output_dir(folder)
+def download_urls(urls: List, tonie_name: str):
+    output_dir = _make_output_dir(tonie_name)
     for i, url in tqdm(enumerate(urls), total=len(urls)):
         video_info = yt_dlp.YoutubeDL().extract_info(
             url=url, download=False
@@ -43,8 +43,8 @@ def download_urls(urls: List, folder: str):
             ydl.download([video_info['webpage_url']])
     return output_dir
 
-def download_playlist(playlist_id: str):
-    output_dir = _make_output_dir(playlist_id)
+def download_playlist(playlist_id: str, tonie_name: str):
+    output_dir = _make_output_dir(tonie_name)
 
     options = {
         **ydl_opts,
